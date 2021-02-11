25dcd8d11d406e3e08e051dbd13efb62c04ba36c
Lilith Wittmann <mail@lilithwittmann.de>
2021-02-11 23:24:01 +0100

    feat(admin): navigation & form list

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,27 @@
         <v-toolbar>
           <v-toolbar-title v-html="pageTitle"></v-toolbar-title>
           <v-spacer></v-spacer>
+
+        <v-toolbar-items  v-if="me">
+
+            <v-btn to="/admin">
+                Admin Panel
+            </v-btn>
+            <v-menu open-on-hover offset-y transition="slide-x-transition" bottom right>
+                <template v-slot:activator="{ on, attrs }">
+                    <v-btn v-bind="attrs" v-on="on">
+                        {{me.firstName}} {{me.lastName}}
+                    </v-btn>
+                </template>
+                <v-list dense>
+                    <v-list-item>
+                       <v-list-item-action  @click="logout()">
+                            <v-list-item-title>Logout</v-list-item-title>
+                        </v-list-item-action>
+                    </v-list-item>
+                </v-list>
+            </v-menu>
+        </v-toolbar-items>
         </v-toolbar>
         <router-view />
       </v-main>
@@ -12,6 +33,8 @@
   </div>
 </template>
 <script>
+      import * as AUTH from "./auth";
+
 export default {
   name: "app",
   components: {},
@@ -19,7 +42,23 @@ export default {
     return {
       pageTitle: process.env.VUE_APP_FORMULARIUM_TITLE
     };
-  }
+  },
+  methods: {
+    logout() {
+      AUTH.logout();
+      this.$router.go("Home");
+    }
+  },
+    apollo: {
+      me: {
+        query() {
+          return require("./graphql/admin/me.gql");
+        },
+        skip() {
+          return AUTH.isLoggedIn() === false;
+        },
+      },
+    }
 };
 </script>
 
--- src/views/admin/AdminDashboard.vue
+++ src/views/admin/AdminDashboard.vue
@@ -1,18 +1,64 @@
 <template>
   <div>
-    <h1 v-if="me">Hello {{ me.firstName }}!</h1>
+      <v-container>
+
+      <v-row dense v-if="allInternalForms">
+        <v-col
+          v-for="item in allInternalForms.edges"
+          v-bind:key="item.node.id"
+          :xl="4"
+          :md="4"
+          :sm="6"
+          :xs="12"
+          :cols="12"
+        >
+          <v-card elevation="2">
+            <v-card-title>{{ item.node.name }}</v-card-title>
+            <v-card-text>
+              {{ item.node.description }}
+            </v-card-text>
+            <v-card-actions>
+              <v-btn text color="primary" :to="`/form/${item.node.id}`">
+                Settings
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                :to="`/editor/${item.node.id}`"
+              >
+                Editor
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+
+        </v-col>
+      </v-row
+      >
+
+      </v-container>
   </div>
+
 </template>
 
 <script>
+    import * as AUTH from "../../auth";
 export default {
   name: "AdminDashboard",
   apollo: {
     me: {
       query() {
-        return require("../../graphql/me.gql");
+        return require("../../graphql/admin/me.gql");
+      },
+      skip () {
+          return AUTH.isLoggedIn() === false;
+        },
+    },
+
+      allInternalForms: {
+          query() {
+            return require("../../graphql/admin/allInternalForms.gql");
+          }
       }
-    }
   }
 };
 </script>
